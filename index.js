23777adfb5895ad7f78929a045678da6a9ce63eb
Andrey Popp <8mayday@gmail.com>
2014-04-14 21:03:24 +0600

    Expose marked and factory function

--- index.js
+++ index.js
@@ -5,59 +5,64 @@ var jsxTransform   = require('react-tools').transform;
 var parseScopeSpec = require('./lib/parseScopeSpec');
 var runtime        = require.resolve('./lib/runtime');
 
-function compile(src, opts) {
-  var compiled = marked(src);
-  var meta = compiled.meta || {};
+function make(marked) {
+  return function compile(src, opts) {
+    var compiled = marked(src);
+    var meta = compiled.meta || {};
 
-  var component = meta.component ?
-    'require(' + JSON.stringify(meta.component) + ')' :
-    opts.component ?
-    'require(' + JSON.stringify(opts.component) + ')' :
-    '_runtime.Reactdown'
+    var component = meta.component ?
+      'require(' + JSON.stringify(meta.component) + ')' :
+      opts.component ?
+      'require(' + JSON.stringify(opts.component) + ')' :
+      '_runtime.Reactdown'
 
-  var scope = {};
+    var scope = {};
 
-  if (meta.scope) {
-    for (var k in meta.scope) {
-      scope[k] = meta.scope[k];
+    if (meta.scope) {
+      for (var k in meta.scope) {
+        scope[k] = meta.scope[k];
+      }
     }
-  }
 
-  if (opts.scope) {
-    var optsScope = parseScopeSpec(opts.scope);
-    for (var n in optsScope) {
-      scope[n] = optsScope[n];
+    if (opts.scope) {
+      var optsScope = parseScopeSpec(opts.scope);
+      for (var n in optsScope) {
+        scope[n] = optsScope[n];
+      }
     }
+
+    delete meta.component;
+    delete meta.scope;
+
+    var scopeCode = [];
+    for (var name in scope) {
+      scopeCode.push('var ' + name +
+          ' = require(' + JSON.stringify(scope[name]) + ');');
+    }
+
+    var code = [
+      '/** @jsx React.DOM */',
+      'var React      = require("react");',
+      '',
+      'var _runtime   = require(' + JSON.stringify(runtime) + ');',
+      'var _Wrapper   = _runtime.Wrapper;',
+      'var _Component = ' + component + ';',
+      '',
+      'module.exports = function create(props) {',
+      '  var _markup    = <_Wrapper>' + compiled.html + '</_Wrapper>;',
+      '  props = _runtime.merge(module.exports.meta, props);',
+      '  return _Component.apply(_Component, [props].concat(_markup));',
+      '};',
+      'module.exports.meta   = ' + JSON.stringify(meta) + ';'
+    ].concat(scopeCode).join('\n');
+
+    code = jsxTransform(code);
+
+    return {code: code, meta: meta};
   }
-
-  delete meta.component;
-  delete meta.scope;
-
-  var scopeCode = [];
-  for (var name in scope) {
-    scopeCode.push('var ' + name +
-        ' = require(' + JSON.stringify(scope[name]) + ');');
-  }
-
-  var code = [
-    '/** @jsx React.DOM */',
-    'var React      = require("react");',
-    '',
-    'var _runtime   = require(' + JSON.stringify(runtime) + ');',
-    'var _Wrapper   = _runtime.Wrapper;',
-    'var _Component = ' + component + ';',
-    '',
-    'module.exports = function create(props) {',
-    '  var _markup    = <_Wrapper>' + compiled.html + '</_Wrapper>;',
-    '  props = _runtime.merge(module.exports.meta, props);',
-    '  return _Component.apply(_Component, [props].concat(_markup));',
-    '};',
-    'module.exports.meta   = ' + JSON.stringify(meta) + ';'
-  ].concat(scopeCode).join('\n');
-
-  code = jsxTransform(code);
-
-  return {code: code, meta: meta};
 }
 
-module.exports = compile;
+module.exports = make(marked);
+module.exports.make = make;
+module.exports.marked = marked;
+module.exports.Renderer = marked.noMeta.Renderer;
