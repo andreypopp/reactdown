f86389f8d69bc348ed7ad9486eda8cf41a807a4d
Andrey Popp <8mayday@gmail.com>
2016-04-07 21:40:13 +0300

    feat(render): add ComponentRef

---
+++ src/ComponentRef.js
@@ -0,0 +1,31 @@
+/**
+ * @copyright 2016-present, Reactdown Team
+ * @flow
+ */
+
+// TODO: More robust regexpes required!
+const PARSE_REF_RE = /^([a-zA-Z0-9_\.\-\/]+)(:([a-zA-Z0-9_]+))?$/;
+const PARSE_NAMED_REF_RE = /^([a-zA-Z0-9_]+)=([a-zA-Z0-9_\.\-\/]+)(:([a-zA-Z0-9_]+))?$/;
+
+export type ComponentRef = {
+  source: string;
+  name: string;
+};
+
+export function parse(ref: string): ?ComponentRef {
+  let match = PARSE_REF_RE.exec(ref);
+  if (!match) {
+    return null;
+  }
+  let [_everything, source, _nothing, name = 'default'] = match;
+  return {source, name};
+}
+
+export function parseNamed(ref: string): ?{id: string; ref: ComponentRef} {
+  let match = PARSE_NAMED_REF_RE.exec(ref);
+  if (!match) {
+    return null;
+  }
+  let [_everything, id, source, _nothing, name = 'default'] = match;
+  return {id, ref: {source, name}};
+}
---
+++ src/__tests__/ComponentRef-test.js
@@ -0,0 +1,88 @@
+/**
+ * @copyright 2016, Andrey Popp <8mayday@gmail.com>
+ * @flow
+ */
+
+import assert from 'assert';
+import {parse, parseNamed} from '../ComponentRef';
+
+declare function describe(description: string, body: any): void;
+declare function it(description: string, body: any): void;
+
+describe('reactdown', function() {
+
+  describe('ComponentRef', function() {
+
+    function generateCases(func, cases) {
+      cases.forEach(c => {
+        let {input, expectation} = c;
+        it(`handles "${input}"`, function() {
+          assert.deepEqual(func(input), expectation);
+        });
+      });
+    }
+
+    describe('parse()', function() {
+
+      generateCases(parse, [
+        {
+          input: 'module',
+          expectation: {source: 'module', name: 'default'}
+        },
+        {
+          input: 'module:',
+          expectation: null
+        },
+        {
+          input: 'module:name',
+          expectation: {source: 'module', name: 'name'}
+        },
+        {
+          input: './module/lib:name',
+          expectation: {source: './module/lib', name: 'name'}
+        },
+        {
+          input: ':name',
+          expectation: null
+        },
+      ]);
+
+    });
+
+    describe('parseNamed()', function() {
+
+      generateCases(parseNamed, [
+        {
+          input: 'module',
+          expectation: null
+        },
+        {
+          input: 'module:',
+          expectation: null
+        },
+        {
+          input: 'module:name',
+          expectation: null
+        },
+        {
+          input: 'id=module',
+          expectation: {id: 'id', ref: {source: 'module', name: 'default'}}
+        },
+        {
+          input: 'id=module:name',
+          expectation: {id: 'id', ref: {source: 'module', name: 'name'}}
+        },
+        {
+          input: 'id=:name',
+          expectation: null
+        },
+        {
+          input: 'id=module:',
+          expectation: null
+        },
+      ]);
+    });
+
+  });
+
+});
