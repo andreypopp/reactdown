f555331abfc53dca542db72c78f207ece543b710
Andrey Popp <8mayday@gmail.com>
2016-04-16 01:35:51 +0300

    feat(config): use json5 for config format

--- src/Config.js
+++ src/Config.js
@@ -8,6 +8,7 @@ import type {DirectiveConfig as DirectiveRenderConfig} from './render';
 
 import fs from 'fs';
 import path from 'path';
+import JSON5 from 'json5';
 import {parseQuery} from 'loader-utils';
 
 export type DirectiveConfig
@@ -51,14 +52,14 @@ export function findConfig(loc: string): {config: Config, sourceList: Array<stri
   while (!(seenPackage && seenConfig) && loc !== path.dirname(loc)) {
     let configLoc = path.join(loc, CONFIG_FILENAME);
     if (fs.existsSync(configLoc)) {
-      config = mergeConfig(config, readJSON(configLoc));
+      config = mergeConfig(config, readJSON(configLoc, JSON5));
       sourceList.push(configLoc);
       seenConfig = true;
     }
 
     let pkgLoc = path.join(loc, PACKAGE_FILENAME);
     if (fs.existsSync(pkgLoc)) {
-      config = mergeConfig(config, readJSON(pkgLoc)['reactdown']);
+      config = mergeConfig(config, readJSON(pkgLoc, JSON)['reactdown']);
       sourceList.push(pkgLoc);
       seenPackage = true;
     }
@@ -76,6 +77,6 @@ export function parseConfigFromQuery(query: string): Config {
   };
 }
 
-function readJSON(loc) {
-  return JSON.parse(fs.readFileSync(loc, {flag: 'r'}).toString('utf8'));
+function readJSON(loc, syntax = JSON) {
+  return syntax.parse(fs.readFileSync(loc, {flag: 'r'}).toString('utf8'));
 }
