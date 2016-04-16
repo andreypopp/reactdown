f716aad23dfd48e07cc8d1b120f2df0156350758
Andrey Popp <8mayday@gmail.com>
2016-04-16 10:30:02 +0300

    doc(webpack): comment code

--- src/webpack.js
+++ src/webpack.js
@@ -10,15 +10,29 @@ import {
   parseConfigFromQuery
 } from './Config';
 
-module.exports = function reactdown(source: string): string {
+/**
+ * Webpack loader for reactdown documents.
+ */
+function reactdown(source: string): string {
   this.cacheable();
+
   let compiler = this._compiler;
+
+  // We read the config once. That means on changes to config one must restart
+  // the compiler.
+  //
+  // TODO: Improve on that, so changes to configration do not require restarting
+  // Webpack compiler.
   if (compiler.__reactdownConfig === undefined) {
     compiler.__reactdownConfig = findConfig(compiler.context).config;
   }
+
   let config = mergeConfig(
     compiler.__reactdownConfig,
     parseConfigFromQuery(this.query)
   );
+
   return renderToString(source, config).code;
-};
+}
+
+module.exports = reactdown;
